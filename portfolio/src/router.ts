72a4eb9e76f592af51a3380f16a675293d2c6851
Seamus Mullan <seamusmullan2023@gmail.com>
2025-06-12 23:52:50 +0100

    maybe fix routing

--- portfolio/src/router.ts
+++ portfolio/src/router.ts
@@ -9,7 +9,7 @@ const routes: RouteRecordRaw[] = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/'),
   routes,
 })
 
--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -5,6 +5,7 @@ import Markdown from 'vite-plugin-md'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     vue({
       include: [/\.vue$/, /\.md$/],
